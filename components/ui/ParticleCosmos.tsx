22af31f5858dd34761136fa8aaad4ad6f63ddd83
srj <mywaysrj@gmail.com>
2026-05-21 11:32:58 +0530

    Fix TypeScript build error for bufferAttribute in ParticleCosmos

--- components/ui/ParticleCosmos.tsx
+++ components/ui/ParticleCosmos.tsx
@@ -78,8 +78,8 @@ export default function ParticleCosmos() {
   return (
     <points ref={pointsRef}>
       <bufferGeometry>
-        <bufferAttribute attach="attributes-position" count={count} array={positions} itemSize={3} />
-        <bufferAttribute attach="attributes-color" count={count} array={colors} itemSize={3} />
+        <bufferAttribute attach="attributes-position" args={[positions, 3]} />
+        <bufferAttribute attach="attributes-color" args={[colors, 3]} />
       </bufferGeometry>
       <pointsMaterial
         size={0.15}
